ee58b5299a7d39ce1afcdc337ffa41a4cac4052f
Paul <paul.fekete@gmail.com>
2015-08-08 16:35:14 +0930

    updating

--- plot4.R
+++ plot4.R
@@ -1,5 +1,30 @@
 # create plot 2
 
+draw_plot3a <- function() {
+  t5 <- transform(dataset,
+                  timepoint=paste(dataset[,1],dataset[,2]))
+  t5$timepoint <- strptime(t5[,"timepoint"],format="%Y-%m-%d %H:%M:%S")
+  plot(t5$timepoint,
+       t5$Sub_metering_1,
+       ylab="Energy sub metering",
+       xlab="",
+       type="l",
+       col="black")
+  
+  lines(t5$timepoint,
+        t5$Sub_metering_2,
+        col="red")
+  
+  lines(t5$timepoint,
+        t5$Sub_metering_3,
+        col="blue")
+  
+  legend("topright",
+         lty="solid",
+         col=c("black","red","blue"),
+         legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+         bty="n")
+}
 draw_plot4a <- function() {
   t5 <- transform(dataset,
                   timepoint=paste(dataset[,1],dataset[,2]))
@@ -28,7 +53,7 @@ draw_plot4 <- function() {
   par(mfcol=c(2,2))
   
   draw_plot2()
-  draw_plot3()
+  draw_plot3a()
   draw_plot4a()
   draw_plot4b()
   
