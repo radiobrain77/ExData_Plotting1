a7254c5b4829af6d79fabbc5316a88a3e7e793a5
Paul <paul.fekete@gmail.com>
2015-08-08 16:06:29 +0930

    updating

--- plot3.R
+++ plot3.R
@@ -2,6 +2,27 @@
 
 png(filename="plot3.png",width=480,height=480)
 
-#plot(dataset$Time,dataset$Global_active_power)
+t5 <- transform(dataset,
+                timepoint=paste(dataset[,1],dataset[,2]))
+t5$timepoint <- strptime(t5[,"timepoint"],format="%Y-%m-%d %H:%M:%S")
+plot(t5$timepoint,
+     t5$Sub_metering_1,
+     ylab="Energy sub metering",
+     xlab="",
+     type="l",
+     col="black")
+
+lines(t5$timepoint,
+      t5$Sub_metering_2,
+       col="red")
+
+lines(t5$timepoint,
+      t5$Sub_metering_3,
+      col="blue")
+
+legend("topright",
+       lty="solid",
+       col=c("black","red","blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off()
